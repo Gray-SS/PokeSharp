9802603c937553c314c743b5f790c58a94830da3
GraySS <doublebump123@gmail.com>
2025-05-11 12:26:48 +0200

    refactor: Enhance ROM loading mechanism and update window title

--- src/Pokemon.DesktopGL/PokemonGame.cs
+++ src/Pokemon.DesktopGL/PokemonGame.cs
@@ -9,6 +9,7 @@ using Pokemon.DesktopGL.Creatures;
 using Pokemon.DesktopGL.World;
 using Pokemon.DesktopGL.Players;
 using Pokemon.DesktopGL.Moves;
+using Pokemon.DesktopGL.ROMs;
 
 namespace Pokemon.DesktopGL;
 
@@ -26,6 +27,7 @@ public class PokemonGame : Game
     public CoroutineManager CoroutineManager { get; private set; }
 
     // Game Properties
+    public ROM ROM { get; }
     public Overworld ActiveWorld { get; set; }
     public PlayerData PlayerData { get; set; }
     public MoveRegistry MoveRegistry { get; private set; }
@@ -39,6 +41,8 @@ public class PokemonGame : Game
     {
         if (Instance != null)
             throw new InvalidOperationException("Impossible to create multiple instances of the game");
+
+        ROM = LoadROM();
 
         Instance = this;
         Graphics = new GraphicsDeviceManager(this);
@@ -61,7 +65,7 @@ public class PokemonGame : Game
 
         PlayerData = new PlayerData();
 
-        WindowManager.SetWindowTitle("Pokemon");
+        WindowManager.SetWindowTitle("PokéSharp");
         WindowManager.SetResolution("1280x720");
 
         base.Initialize();
@@ -103,107 +107,14 @@ public class PokemonGame : Game
         ScreenManager.Draw(gameTime);
     }
 
-    // private void DrawGUI(GameTime gameTime)
-    // {
-    //     _imGuiRenderer.BeginLayout(gameTime);
+    private static ROM LoadROM()
+    {
+        string[] args = Environment.GetCommandLineArgs();
 
-    //     if (ImGui.Begin("Tilesets Manager"))
-    //     {
-    //         if (_tilemap != null)
-    //         {
-    //             ImGui.Text($"Map Size: {_tilemap.MapWidth}x{_tilemap.MapHeight}");
-    //             ImGui.Text($"Total Tiles: {_tilemap.Tiles.Length}");
+        if (args.Length < 2)
+            throw new NotImplementedException("You MUST pass the ROM path when running the project. Usage: dotnet run <rom_path>");
 
-    //             if (ImGui.BeginChild("TilesetViewer", new System.Numerics.Vector2(0, 300)))
-    //             {
-    //                 float tileDisplaySize = 64.0f;
-    //                 float padding = 5.0f;
-    //                 float windowWidth = ImGui.GetWindowWidth();
-    //                 int tilesPerRow = Math.Max(1, (int)((windowWidth - padding) / (tileDisplaySize + padding)));
-
-    //                 for (int i = 0; i < _tilemap.Tiles.Length; i++)
-    //                 {
-    //                     int column = i % tilesPerRow;
-
-    //                     if (column > 0)
-    //                         ImGui.SameLine();
-
-    //                     ImGui.BeginGroup();
-
-    //                     Sprite tile = _tilemap.Tiles[i];
-    //                     IntPtr textureId = _imGuiRenderer.BindTexture(tile.Texture);
-
-    //                     System.Numerics.Vector2 uv0 = new System.Numerics.Vector2(
-    //                         (float)tile.Bounds.X / tile.Texture.Width,
-    //                         (float)tile.Bounds.Y / tile.Texture.Height);
-
-    //                     System.Numerics.Vector2 uv1 = new System.Numerics.Vector2(
-    //                         (float)(tile.Bounds.X + tile.Bounds.Width) / tile.Texture.Width,
-    //                         (float)(tile.Bounds.Y + tile.Bounds.Height) / tile.Texture.Height);
-
-    //                     ImGui.Image(textureId,
-    //                                 new System.Numerics.Vector2(tileDisplaySize, tileDisplaySize),
-    //                                 uv0, uv1);
-
-    //                     string label = $"ID: {i}";
-    //                     float textWidth = ImGui.CalcTextSize(label).X;
-    //                     float centerPos = (tileDisplaySize - textWidth) * 0.5f;
-    //                     ImGui.SetCursorPosX(ImGui.GetCursorPosX() + centerPos);
-    //                     ImGui.Text(label);
-
-    //                     ImGui.EndGroup();
-
-    //                     if (ImGui.IsItemHovered())
-    //                     {
-    //                         ImGui.BeginTooltip();
-    //                         ImGui.Text($"Tile ID: {i}");
-    //                         ImGui.Text($"Source: {tile.Bounds.X},{tile.Bounds.Y},{tile.Bounds.Width},{tile.Bounds.Height}");
-    //                         ImGui.EndTooltip();
-    //                     }
-    //                 }
-    //             }
-    //             ImGui.EndChild();
-
-    //             if (ImGui.CollapsingHeader("Tile Usage"))
-    //             {
-    //                 var tileUsage = new Dictionary<int, int>();
-    //                 for (int i = 0; i < _tilemap.Data.Length; i++)
-    //                 {
-    //                     int tileId = _tilemap.Data[i];
-    //                     if (!tileUsage.ContainsKey(tileId))
-    //                         tileUsage[tileId] = 0;
-    //                     tileUsage[tileId]++;
-    //                 }
-
-    //                 if (ImGui.BeginTable("TileUsage", 2))
-    //                 {
-    //                     ImGui.TableSetupColumn("Tile ID");
-    //                     ImGui.TableSetupColumn("Usage Count");
-    //                     ImGui.TableHeadersRow();
-
-    //                     foreach (var pair in tileUsage.OrderBy(p => p.Key))
-    //                     {
-    //                         ImGui.TableNextRow();
-
-    //                         ImGui.TableSetColumnIndex(0);
-    //                         ImGui.Text($"{pair.Key}");
-
-    //                         ImGui.TableSetColumnIndex(1);
-    //                         ImGui.Text($"{pair.Value}");
-    //                     }
-
-    //                     ImGui.EndTable();
-    //                 }
-    //             }
-    //         }
-    //         else
-    //         {
-    //             ImGui.Text("No tilemap loaded.");
-    //         }
-
-    //         ImGui.End();
-    //     }
-
-    //     _imGuiRenderer.EndLayout();
-    // }
+        string romPath = args[1];
+        return ROM.LoadFromPath(romPath);
+    }
 }
