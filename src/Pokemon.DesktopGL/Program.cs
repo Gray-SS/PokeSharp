192dd4d563025ef75a150b8a102eae2398ea149c
GraySS <doublebump123@gmail.com>
2025-05-12 15:56:59 +0200

    refactor: Enhance ROM path handling and user prompts in Program.cs

--- src/Pokemon.DesktopGL/Program.cs
+++ src/Pokemon.DesktopGL/Program.cs
@@ -1,4 +1,53 @@
-﻿using Pokemon.DesktopGL;
+﻿using System;
+using System.IO;
+using Pokemon.DesktopGL;
 
-using var game = new PokemonGame();
+string romPath = null;
+
+if (args.Length < 1)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("No ROM path provided. The game will run in limited mode.");
+
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Usage: dotnet run <rom_path>");
+}
+else
+{
+    romPath = args[0];
+
+    if (!File.Exists(romPath))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: The specified ROM path doesn't exists: {romPath}.");
+        Console.ResetColor();
+        Console.WriteLine("Usage: dotnet run <rom_path>");
+
+        // Exit with error status code if the specified ROM path doesn't exists
+        Environment.Exit(1);
+    }
+
+    string extension = Path.GetExtension(romPath).ToLower();
+    if (extension != ".gba")
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Warning: The file doesn't look like to be a supported ROM.");
+        Console.ResetColor();
+        Console.WriteLine("Would you still like to continue? (Y/N)");
+
+        ConsoleKey key;
+        Console.Write("> ");
+        do key = Console.ReadKey(true).Key;
+        while (key != ConsoleKey.Y && key != ConsoleKey.N);
+        Console.WriteLine(key);
+
+        if (key == ConsoleKey.N)
+        {
+            Console.WriteLine("Exiting...");
+            Environment.Exit(1);
+        }
+    }
+}
+
+using var game = new PokemonGame(romPath);
 game.Run();
