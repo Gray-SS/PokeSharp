3bcb07a59295ed3e54acc09f001cfd3912f3e196
GraySS <doublebump123@gmail.com>
2025-06-12 18:36:21 +0200

    feat: add global using directives for System.Drawing and System.Numerics types

--- src/Editor/PokeLab.Presentation.ImGui/GlobalUsings.cs
+++ src/Editor/PokeLab.Presentation.ImGui/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using Gui = ImGuiNET.ImGui;
+global using SColor = System.Drawing.Color;
+global using NVec2 = System.Numerics.Vector2;
+global using NVec3 = System.Numerics.Vector3;
+global using NVec4 = System.Numerics.Vector4;
