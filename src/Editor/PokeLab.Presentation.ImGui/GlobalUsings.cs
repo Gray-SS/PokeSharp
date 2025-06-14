7cc0ad09a67b2634bb9295d3d0b5f11483cebbf1
GraySS <doublebump123@gmail.com>
2025-06-14 02:56:18 +0200

    fix: correct global using alias for Color to use Microsoft.Xna.Framework.Color

--- src/Editor/PokeLab.Presentation.ImGui/GlobalUsings.cs
+++ src/Editor/PokeLab.Presentation.ImGui/GlobalUsings.cs
@@ -1,5 +1,5 @@
 global using Gui = ImGuiNET.ImGui;
-global using SColor = System.Drawing.Color;
+global using Color = Microsoft.Xna.Framework.Color;
 global using NVec2 = System.Numerics.Vector2;
 global using NVec3 = System.Numerics.Vector3;
 global using NVec4 = System.Numerics.Vector4;
