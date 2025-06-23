7e63d1d17c52b59d137b9875adce220337e4907b
GraySS <doublebump123@gmail.com>
2025-06-23 14:48:35 +0200

    refactor: reorganize service registration and command setup in Program.cs; improve clarity and maintainability

--- src/Tools/PokeTools.Assets.CLI/Program.cs
+++ src/Tools/PokeTools.Assets.CLI/Program.cs
@@ -1,20 +1,39 @@
 ﻿using Spectre.Console.Cli;
+using PokeCore.Common.Extensions;
 using PokeCore.DependencyInjection.Ninject;
 using PokeCore.DependencyInjection.Abstractions.Extensions;
 using PokeTools.Assets.CLI;
 using PokeTools.Assets.CLI.Services;
 using PokeTools.Assets.CLI.Commands;
+using PokeTools.Assets.Extensions;
+using PokeCore.Logging.Extensions;
+using PokeTools.Assets;
+using PokeCore.DependencyInjection.Abstractions;
+using PokeCore.Hosting.Abstractions;
 
 var services = new NinjectServiceCollections();
+services.AddSingleton<IApp>();
+services.AddSingleton<IServiceResolver>(sc => sc);
+services.AddSingleton<IServiceProvider>(sc => sc);
+services.AddPokeCore();
+services.AddPokeToolsAssets();
+
 services.AddTransient<ICliConsole, CliConsole>();
+services.ConfigureLogging(x => x.UseEmptyLogger());
 
 var app = new CommandApp(new TypeRegistrar(services));
 app.Configure(x =>
 {
+// #if DEBUG
+//     x.PropagateExceptions();
+//     x.ValidateExamples();
+// #endif
+
     x.SetApplicationName("poketools");
     x.SetApplicationVersion("PokéTools v0.0.1");
 
-    x.AddCommand<BuildCommand>("build").WithAlias("test");
+    x.AddCommand<BuildCommand>("build");
+    x.AddCommand<ImportCommand>("import");
 });
 
-await app.RunAsync(args);
+Environment.ExitCode = await app.RunAsync(args);
