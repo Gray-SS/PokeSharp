72e5923f383ff3eead167a39371f170093b86ab4
GraySS <doublebump123@gmail.com>
2025-06-23 14:44:36 +0200

    refactor: convert ServiceDescriptor to an abstract record for improved type safety and clarity

--- src/Core/PokeCore.DependencyInjection.Abstractions/ServiceDescriptor.cs
+++ src/Core/PokeCore.DependencyInjection.Abstractions/ServiceDescriptor.cs
@@ -1,31 +1,23 @@
 namespace PokeCore.DependencyInjection.Abstractions;
 
-public sealed class ServiceDescriptor
+public abstract record class ServiceDescriptor(ServiceLifetime Lifetime, Type ServiceType)
 {
-    public ServiceLifetime Lifetime { get; }
-    public Type ServiceType { get; }
-    public Type? ImplementationType { get; }
-    public object? ImplementationInstance { get; }
-    public Func<IServiceResolver, object>? ImplementationFactory { get; }
+    public sealed record class TypeServiceDescriptor(ServiceLifetime Lifetime, Type ServiceType, Type ImplementationType) : ServiceDescriptor(Lifetime, ServiceType);
+    public sealed record class InstanceServiceDescriptor(ServiceLifetime Lifetime, Type ServiceType, object ImplementationInstance) : ServiceDescriptor(Lifetime, ServiceType);
+    public sealed record class FactoryServiceDescriptor(ServiceLifetime Lifetime, Type ServiceType, Func<IServiceResolver, object> ImplementationFactory) : ServiceDescriptor(Lifetime, ServiceType);
 
-    public ServiceDescriptor(Type serviceType, Type implType, ServiceLifetime lifetime)
+    public static ServiceDescriptor FromType(Type serviceType, Type implType, ServiceLifetime lifetime)
     {
-        ServiceType = serviceType;
-        ImplementationType = implType;
-        Lifetime = lifetime;
+        return new TypeServiceDescriptor(lifetime, serviceType, implType);
     }
 
-    public ServiceDescriptor(Type serviceType, object implInstance, ServiceLifetime lifetime)
+    public static ServiceDescriptor FromFactory(Type serviceType, Func<IServiceResolver, object> factory, ServiceLifetime lifetime)
     {
-        ServiceType = serviceType;
-        ImplementationInstance = implInstance;
-        Lifetime = lifetime;
+        return new FactoryServiceDescriptor(lifetime, serviceType, factory);
     }
 
-    public ServiceDescriptor(Type serviceType, Func<IServiceProvider, object> implFactory, ServiceLifetime lifetime)
+    public static ServiceDescriptor FromInstance(Type serviceType, object instance, ServiceLifetime lifetime)
     {
-        ServiceType = serviceType;
-        ImplementationFactory = implFactory;
-        Lifetime = lifetime;
+        return new InstanceServiceDescriptor(lifetime, serviceType, instance);
     }
 }
